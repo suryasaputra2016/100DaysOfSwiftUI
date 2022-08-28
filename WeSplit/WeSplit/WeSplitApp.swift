edde0bdcf9278c340fe123c3fc7b2c128a179b05
surya saputra <sayasuryasaputra@gmail.com>
2022-08-28 20:15:00 +0700

    adding WeSplit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Asti Widiyaningsih on 28/08/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
