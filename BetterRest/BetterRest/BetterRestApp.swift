06909712e0003f12a3bfaab1f03056baaa7ec9e6
surya saputra <sayasuryasaputra@gmail.com>
2022-09-10 06:24:02 +0700

    day 26 machine learning

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Asti Widiyaningsih on 10/09/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
